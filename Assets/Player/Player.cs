c7e623e176c3ca5b39f1c9cf68ee8da1d0892983
jprischak <jprischak@gmail.com>
2018-02-18 22:13:51 -0500

    Player Healthbar added

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    /**
+    *  VARIABLES
+    * */
+
+    private float currentHealthPoints = 100f;
+    private float maxHealthPoints = 100f;
+    
+
+
+
+
+
+    /**
+    *  CLASS FUNCTIONS
+    * */
+    private void Start()
+    {
+
+    }
+
+
+    private void Update()
+    {
+
+    }
+
+
+    
+
+
+
+
+
+    /**
+    *  FUNCTIONS
+    * */
+    public float HealthAsPercentage()
+    {
+        return currentHealthPoints / maxHealthPoints;
+    }
+}
