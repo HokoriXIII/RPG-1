802ed14a341223bc079a4a8a461c90033d3e3af8
JASONLAPTOP\Jason <jprischak@gmail.com>
2018-02-25 20:39:41 -0500

    Fix camera working, begining to work on follow camera functionality

--- Assets/Camera & UI/CameraFollow.cs
+++ Assets/Camera & UI/CameraFollow.cs
@@ -13,8 +13,12 @@ public class CameraFollow : MonoBehaviour {
 
     private GameObject player;
     private GameObject myCamera;
-    private Vector3 savedRotation;
-    
+
+    // Used to save values for the fixed camera setup
+    private Quaternion savedRotation;
+    private Vector3 savedPositionOffset;
+
+
 
 
 
@@ -26,23 +30,34 @@ public class CameraFollow : MonoBehaviour {
     {
         player = GameObject.FindGameObjectWithTag("Player");
         myCamera = GameObject.FindGameObjectWithTag("MainCamera");
-        savedRotation = myCamera.transform.rotation.eulerAngles;
+
+        savedRotation = myCamera.transform.rotation;
+        savedPositionOffset = myCamera.transform.position;
     }
 
 
     private void Update()
     {
-
+        
     }
 
     private void LateUpdate()
     {
+        // Move our camera arm to the location of our player
         transform.position = player.transform.position;
 
-        if (!fixedCamera)
-            myCamera.transform.rotation = player.transform.rotation;
+
+
+        if (fixedCamera)
+        {      
+            myCamera.transform.rotation = savedRotation;
+            myCamera.transform.position = transform.position + savedPositionOffset;
+        }
         else
-            myCamera.transform.eulerAngles  = savedRotation;
+        {
+            myCamera.transform.rotation = Quaternion.Lerp(myCamera.transform.rotation, transform.rotation, (1f * Time.deltaTime));
+
+        }
     }
 
 
